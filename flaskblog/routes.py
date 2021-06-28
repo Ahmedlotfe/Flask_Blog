ceaf97d79d91a1b12067fafbd64e2bc8cc5c2d39
Ahmedlotfe <ahmedlotfe132@gmail.com>
2021-06-28 15:51:30 +0200

    Added project files

---
+++ flaskblog/routes.py
@@ -0,0 +1,168 @@
+import os
+import secrets
+from PIL import Image
+from flaskblog.models import User, Post
+from flask import render_template, flash, redirect, url_for, request, abort
+from flaskblog import app, db, bcrypt
+from .forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_login import login_user, logout_user, current_user, login_required
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page = 5, page=page)
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password_hash=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your Account has been created! You are now able to login",
+              category='success')
+        return redirect(url_for('login_page'))
+
+    return render_template('register.html', form=form, title='Register')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home_page'))
+        else:
+            flash('Login Unsuccessful, Please check email and password',
+                  category='danger')
+
+    return render_template('login.html', form=form, title='Login')
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    return redirect(url_for('home_page'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_fn)
+
+    picture_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(picture_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account_page():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", category='success')
+        return redirect(url_for('account_page'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, user_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post has been created!', category='success')
+        return redirect(url_for('home_page'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Done! Your post has been updated', category='success')
+        return redirect(url_for('post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    
+    db.session.delete(post)
+    db.session.commit()
+    flash('Done! Your post has been deleted', category='success')
+    return redirect(url_for('home_page'))
+
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(per_page = 5, page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
---
+++ run.py
@@ -0,0 +1,6 @@
+from flaskblog import app
+
+names = ['A', 'P']
+
+if __name__ == '__main__':
+    app.run(debug=True)
